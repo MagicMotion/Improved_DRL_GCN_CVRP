bad813b7cc4a7f5b84e5119d2ae9e61c3c264015
MagicMotion <MagicMotion2003@proton.me>
2022-01-28 07:50:18 +0000

    updating readme.

--- Data.py
+++ Data.py
@@ -129,4 +129,5 @@ class DataManager(object):
                 else:
                     path_matrix = np.concatenate((path_matrix, np.expand_dims(file['shortest_path_matrix'], axis=0)),
                                                  axis=0)
-                    demand = np.concatenate((demand
+                    demand = np.concatenate((demand, np.expand_dims(file['demand'], axis=0)), axis=0)
+                    coordina
